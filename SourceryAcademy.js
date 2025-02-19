68afb3778ab3f191b734d2207f9d252b5b593c6f
Jonas Laurinavičius <jonaslaurinavicius1@gmail.com>
2025-02-19 12:16:54 +0200

    Optimized code
    Changed loops and if statements. Also fixed decompress issue when number of characters is double digits

--- SourceryAcademy.js
+++ SourceryAcademy.js
@@ -1,44 +1,44 @@
-let input = "aaabbb"
+let input = "aaaaaaaaaaabbb"
 
 function stringCompress(input){
+    if (!input) return "";
+  
     let output = "";
-    if(input.length == 0){
-        return output;
-    }
-    else{
-        let count = 1;
-        for(let i = 0; i < input.length; i++){
-            if(i == input.length -1){
-                return output += input[i] + count;
-            }
-            else if(input[i] == input[i+1]){
-                count++;
-            }
-            else{
-                output += input[i] + count;
-                count = 1;
-            }
-        }
-    }
-    return output;
-}
+    let count = 1
 
-function stringDecompress(input){
-    let output = "";
-    if(input.length == 0){
-        return output;
-    }
-    for(let i = 0; i < input.length; i++){
-        if(input[i] >= '0' && input[i] <= '9'){
-            let letter = input[i-1];
-            let count = parseInt(input[i]);
-            for(j = 0; j < count; j++){
-                output += letter;
-            }
+    for(let i = 1; i < input.length; i++){
+        if(input[i] === input[i-1]){
+            count++;
+        } else {
+            output += input[i-1] + count;
+            count = 1;
         }
     }
+    
+    output += input[input.length - 1] + count;
+    return output;}
+
+function stringDecompress(input) {
+    if (!input) return "";
+    
+    let output = "";
+    let i = 0;
+    
+    while (i < input.length) {
+      const char = input[i++];
+      let countStr = "";
+      
+      while (i < input.length && input[i] >= '0' && input[i] <= '9') {
+        countStr += input[i++];
+      }
+      
+      const count = parseInt(countStr, 10);
+      output += char.repeat(count);
+    }
+    
     return output;
-}
+  }
+  
 
 compressOutput = stringCompress(input);
 decompressOutput = stringDecompress(compressOutput);
